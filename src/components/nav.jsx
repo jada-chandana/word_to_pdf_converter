3611ba84b65878f98cad993c3b154b79e2f1a6f5
Chandana jada <chandanaj405@gmail.com>
2025-12-16 16:02:45 +0530

    Change navbar title from 'Aspire Tekhub Solutions' to 'Word To PDF'

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,9 +7,9 @@ function Navbar() {
       <div className="navbar">
         <div className="navbar-content">
           <img src={logo}/>
-          <h1 className="head">Aspire Tekhub Solutions</h1>
+          <h1 className="head">Word To PDF</h1>
           <h1 className="logo">
-            Word<span className="highlight">To</span>PDF
+           Aspire Tekhub Solutions
           </h1>
         </div>
       </div>
